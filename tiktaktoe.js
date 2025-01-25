16e1fde981d8491be7025788ac8be360adbcf40c
king7hills <haasjh1@gmail.com>
2025-01-25 17:49:01 -0500

    created lots of functions. Need to commit sooner.

--- tiktaktoe.js
+++ tiktaktoe.js
@@ -10,38 +10,149 @@
 //     }
 // })()
 
-const gameboard = (function() {
-    const board = [];
-    const rows = 3;
-    const columns = 3;
+const gameBoard = {
+    board: [],
+    rows: 3,
+    columns: 3,
 
-    function createArray () {
-    for (let i = 0; i<rows; i++) {
-        board[i] = [];
-        for (let j = 0; j<columns; j++) {
-            board[i][j] = '';
+    createBoard: function () {
+    for (let i = 0; i < this.rows; i++) {
+        this.board[i] = [];
+        for (let j = 0; j < this.columns; j++) {
+            this.board[i][j] = '';
         }}
-    _render();
-    }
+    this.render();
+    },
     
-    function _render() {
-        console.log(board);
-    }
+    render: function () {
+        console.log(this.board);
+    },
 
-    function selectPosition (row, column) {
-        const position = board[row][column]
-    }
+    turnSuccess: false,
+
+    getSuccess: function () {
+        return this.turnSuccess;
+    },
     
-    return createArray;
-})
+    mark: function(row, column, marker) {
+        var position = this.board[row-1][column-1];
+        if (position == "") {
+            this.board[row-1][column-1] = marker;
+            this.render();
+            this.turnSuccess = true;
+        } else {
+            this.turnSuccess = false;
+            console.log("Try another position");
+        }
+    },
+
+    winStatus: false,
+
+    setWinStatus: function (marker) {
+        const threeMarks = marker.repeat(3);
+
+        for (let i = 0; i < this.rows; i++) {
+            //row win condition
+            let sameRowValues = this.board[i].join('');
+            if (sameRowValues == threeMarks)
+            {return this.winStatus = true;};
+        };
+
+            //column win condition
+        let column1 = '';
+        let column2 = '';
+        let column3 = '';
+        column1 = this.board[0][0] + this.board[0][1] + this.board[0][2];
+        column2 = this.board[1][0] + this.board[1][1] + this.board[1][2];
+        column3 = this.board[2][0] + this.board[2][1] + this.board[2][2];
+        
+        if (column1 == threeMarks || column2 == threeMarks || column3 == threeMarks)
+            {return this.winStatus = true;
+        } else {};
+        
+        //diagonal win conditions
+        let diagonal1 = this.board[1][1]+this.board[0][0]+this.board[2][2];
+        let diagonal2 = this.board[1][1]+this.board[0][2]+this.board[2][0];
+
+        if (diagonal1 == threeMarks || diagonal2 == threeMarks) {
+            return this.winStatus = true;
+        };
+    },
+
+    checkForWin: function (marker) {
+        this.setWinStatus(marker);
+        return this.winStatus;
+    }
+};
 
 
-const playerModule = (function() {
-    const player = function Player (name, token) {
+
+function playGame(player1Name, player2Name) {
+    const player = function Player (name, marker) {
         this.name = name;
-        this.token = token;
+        this.marker = marker;
+    };
+
+    const player1 = new player(player1Name, 'X');
+    const player2 = new player(player2Name, 'O');
+
+    let turnCount = 0;
+    let winStatus = false;
+
+    //Limits running win check until someone can possibly win
+    function checkTurn (marker) {
+        if (turnCount < 5) {
+            nextTurn();
+        } else if (turnCount >= 5 && turnCount < 9) {
+            winStatus = gameBoard.checkForWin(marker);
+            console.log(winStatus);
+            if (winStatus == false) {
+                nextTurn();
+            } else if (winStatus == true) {
+                endGame(true);
+            }
+        } else if (turnCount == 9) {
+            if (winStatus == true) {
+                endGame(true);
+            } else if (winStatus == false) {
+                endGame(false);
+            }
+        };
+    };
+
+    let lastTurn = '';
+
+    //turn logic
+    function nextTurn () {
+        if (lastTurn == '' || lastTurn == player2.name) {
+            takeTurn(player1);
+        } else if (lastTurn == player1.name) {
+            takeTurn(player2);
+        };
+    };
+
+    function endGame (value) {
+        if (value == true) {
+            console.log(`Game over. ${lastTurn} wins!`)
+        } else console.log('Game over. Cat game! No winners!');
     }
 
-    const player1 = new player('Player 1', 'X')
-    const player2 = new player('Player 2', 'O')
-})
+    function takeTurn (player) {
+        console.log(`${player.name}'s turn.`);
+        let coordinates = prompt("Input position separated by comma (1-3,1-3): ");
+        const turnCoordinates = coordinates.split(',');
+
+        gameBoard.mark(turnCoordinates[0], turnCoordinates[1], player.marker);
+        const status = gameBoard.getSuccess();
+        if (status == true) {
+            turnCount++;
+            lastTurn = player.name;
+            checkTurn(player.marker);
+        } else if (status == false) {
+            takeTurn(player);
+        }
+    };
+
+    gameBoard.createBoard()
+    nextTurn();
+}
